4422be0036c1b6b2f49608bd279ab87151e2c9a2
Danny Myles <mylesd2@ferris.edu>
2019-09-30 19:29:19 -0400

    Created Simple Game With Scoreboard
    +Sphere (Prefab)
    +Cylinder
    +Scoreboard

---
+++ Assets/Myles/Scripts/CannonController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class CannonController : MonoBehaviour
+{
+    MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+    }
+
+    
+    void Update()
+    {
+        meshRenderer.material.color = new Color(0, 0, 0);
+    }
+}
