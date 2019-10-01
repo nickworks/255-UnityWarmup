2110ea6c1722636d75b152a6a880a9875216e303
nate breu <breun@ferris.edu>
2019-10-01 09:05:56 -0400

    Homework 9/24

---
+++ Assets/Breu/Scripts/BreuPickupController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreuPickupController : MonoBehaviour
+{
+
+    public float spinSpeed = 1;
+    public float rotationSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(rotationSpeed, spinSpeed, rotationSpeed) * Time.deltaTime);
+    }
+}
